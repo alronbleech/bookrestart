8bac025c02c315f3c5229c5b3803edd6d1a11c20
alronbleech <126852030+alronbleech@users.noreply.github.com>
2023-05-04 08:50:27 +0000

    [Finish]22

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   get 'books/index'
   get 'books/show'
   get 'books/edit'
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about'
   resources :users, only: [:index,:show,:edit,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
