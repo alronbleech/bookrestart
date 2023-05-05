a7317d136dcd83d97ae789a3e86efa862eee1241
alronbleech <126852030+alronbleech@users.noreply.github.com>
2023-05-05 06:26:13 +0000

    [Finish]25

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   devise_for :users
   root to: 'homes#top'
   get 'homes/about'
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only:[:create,:destroy]
